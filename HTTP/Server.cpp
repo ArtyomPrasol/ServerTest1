c61fa54823f09491903c46dbba8ae1bf97a80c41
ArtyomPrasol <112947624+ArtyomPrasol@users.noreply.github.com>
2022-12-19 14:41:48 +0300

    Update Server.cpp

--- HTTP/Server.cpp
+++ HTTP/Server.cpp
@@ -9,9 +9,12 @@ using namespace std;
 
 int main() {
 	WSADATA wsaData;
-	addrinfo hints, *addr;
+	struct addrinfo* addr = NULL;
+	struct addrinfo hints;
+	ZeroMemory(&hints, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_flags = AI_PASSIVE;
+	hints.ai_protocol = IPPROTO_TCP;
 	hints.ai_socktype = SOCK_STREAM;
 
 	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -20,8 +23,7 @@ int main() {
 		return result;
 	}
 
-	ZeroMemory(&hints, sizeof(hints));
-	if (result = getaddrinfo("127.0.0.1", "8000", &hints, &addr)) {
+	if ((result = getaddrinfo("localhost", "8000", &hints, &addr))!=0) {
 		cerr << "GetAddrInfo Error: " << result << "\n";
 		WSACleanup();
 		return 1;
@@ -78,10 +80,14 @@ int main() {
 		else if (result > 0)
 		{
 			buf[result] = '\0';
-			response_body << "<title> Test C++ HTTP Server</title>\n" << "<h1>Test page</h1>\n" << "<p>This is body of the test page...</p>\n" << "<h2>Request headers</h2>\n" << "<pre>" << buf << "</pre\n"
+			response_body << "<title> Test C++ HTTP Server</title>\n" 
+				<< "<h1>Test page</h1>\n" 
+				<< "<p>This is body of the test page...</p>\n" 
+				<< "<h2>Request headers</h2>\n" 
+				<< "<pre>" << buf << "</pre\n>" 
 				<< "<em><small>Test C++ Http Server </small></em>\n";
 
-			response << "HTTP/1.1 200 OK\r\n" << "Version: HTTP/1.1\r\n" << "Content - Type: text/html; charset=utf-8\r\n" << "Content-Length: " << response_body.str().length() << "\r\n\r\n" << response.str();
+			response << "HTTP/1.1 200 OK\r\n" << "Version: HTTP/1.1\r\n" << "Content - Type: text/html; charset=utf-8\r\n" << "Content-Length: " << response_body.str().length() << "\r\n\r\n" << response_body.str();
 
 			result = send(client_socket, response.str().c_str(), response.str().length(), 0);
 
